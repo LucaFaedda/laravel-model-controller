bdbaf38d994ec859d1aec9b22a3789a9568530cf
LucaFaedda <lucakarl91@gmail.com>
2023-02-20 17:23:49 +0100

    creato model in app

---
+++ app/Models/Comicl.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comicl extends Model
+{
+    use HasFactory;
+}
